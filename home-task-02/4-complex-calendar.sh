746e1364b183bbfe4102671a1c5a388402a283f3
KidMarten <>
2025-04-09 16:53:04 +0300

    completes second home task

---
+++ home-task-02/4-complex-calendar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Скрипт, который спрашивает день недели, уточняет число и выводит все на одной строке
+
+read -p "Enter today's day of week: " dow
+read -p "Enter today's date: " date
+
+if [[ -n "$dow" && -n "$date" ]]; then
+    echo $dow $date
+else
+    echo "Input is empty"
+    exit 0
+fi
