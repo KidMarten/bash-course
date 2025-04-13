27b6c31cdbbb89a356f446421f795d1990dc88b4
Pavel <>
2025-04-13 11:02:31 +0300

    fix: fixed shebang

--- home-task-06/04-error-fix.sh
+++ home-task-06/04-error-fix.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 # Скрипт, который создает файлы с содержимым строк из /etc/os-release (с отправкой ошибок в /dev/null)
 while read line; do ((++i)); echo "line$i,$line,"; done < /etc/os-release | xargs -n 2 -d ',' sh -c 'name=$(echo $0 | tr -cd "[:print:]\n"); content=$1; echo $content > $name' 2>/dev/null
