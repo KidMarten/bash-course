46cd580a27cff614439ee6f3012b0115e8d99269
Pavel <>
2025-04-15 13:34:07 +0300

    fix: fixed all files

--- home-task-08/01-users.sh
+++ home-task-08/01-users.sh
@@ -6,17 +6,19 @@ add_user () {
   local username=$1
   local uid=$2
   local hostname=$3
-  ssh $hostname "echo $PASSWORD | sudo -S useradd -m -s /bin/bash -u $uid $username"
+  local pass=$4
+  ssh $hostname "echo $pass | sudo -S useradd -m -s /bin/bash -u $uid $username"
 }
 
 read -p "Enter user name: " username
 read -p "Enter user ID: " uid
+read -p "Enter password: " pass
 
-if [[ -z $username || -z $uid ]]; then
-  echo "Input is empty. Enter Username and UID"
+if [[ -z $username || -z $uid || -z $uid ]]; then
+  echo "Input is empty. Enter Username, UID and password"
   exit 1
 fi
 
-while IFS= read -r $host; do
-    add_user $username $uid $host
+while IFS= read -r host; do
+    add_user $username $uid $host $pass
 done < home-task-08/hosts.txt
--- home-task-08/02-backup.sh
+++ home-task-08/02-backup.sh
@@ -2,7 +2,7 @@
 
 # Скрипт для создания резервных копий на другой машине
 
-read -p "Enter a list of dirs you want to make a backup of: " backup_dirs
+backup_dirs=($@)
 
 if [ ${#backup_dirs} -eq 0 ]; then
   echo List of dirs is empty
@@ -13,9 +13,9 @@ dest_dir="/mnt/backup/"
 dest_server="192.168.0.55"
 backup_time=$(date "+%Y-%m-%d")
 
-for dir in $backup_dirs; do
+for dir in ${backup_dirs[@]}; do
   arc_name=$(echo ${dir}.${backup_time}.tar.gz)
   tar -czvf /tmp/$arc_name $dir
-  scp $arc_name $dest_server:$dest_dir
+  scp /tmp/$arc_name $dest_server:$dest_dir
   rm /tmp/$arc_name
 done
